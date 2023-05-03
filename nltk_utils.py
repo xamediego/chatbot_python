b2bb90e93ea40b33c45749d3b6e63ac21a44f36d
BigGuy4U <88188017+xamediego@users.noreply.github.com>
2023-05-03 15:16:44 +0200

    initial

---
+++ nltk_utils.py
@@ -0,0 +1,25 @@
+import nltk
+from nltk.stem.porter import PorterStemmer
+import numpy as np
+
+# nltk.download('punkt')
+stemmer = PorterStemmer()
+
+
+def tokenize(sentence):
+    return nltk.wordpunct_tokenize(sentence)
+
+
+def stem(word):
+    return stemmer.stem(word.lower())
+
+
+def bag_of_words(tokenized_sentence, all_words):
+    tokenized_sentence = [stem(w) for w in tokenized_sentence]
+
+    bag = np.zeros(len(all_words), dtype=np.float32)
+    for index, word in enumerate(all_words):
+        if word in tokenized_sentence:
+            bag[index] = 1.0
+
+    return bag
